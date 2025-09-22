05ba9d3b33d1e8cfc7479120451b30acd46dd808
Dua-Fatima-khan <officialduafatima64648@gmail.com>
2025-09-23 03:37:30 +0500

    width

--- src/screens/homepage/Section2/index.tsx
+++ src/screens/homepage/Section2/index.tsx
@@ -25,26 +25,26 @@ const AboutSection = () => {
       </motion.div>
       {/* Services */}
       <div className="space-y-8 md:max-w-[90%]  cursor-pointer w-full p-">
-        {" "}
+
         {services.map((service) => (
           <div
             key={service.id}
             className="p-6 border-b-2 border-[#058bae] bg-[#011217cc] shadow-[0_0_10px_rgba(5,139,174,0.2)] hover:shadow-[0_0_20px_rgba(5,139,174,0.5)] hover:border-[#05C0E0] transition-all duration-300 ease-in-out group"
           >
-            {" "}
+
             <div className="w-full flex gap-8 items-center">
-              {" "}
+    
               <p className="text-[#058bae] text-2xl font-bold hover:text-[#05C0E0] group-hover:scale-110 transition-transform duration-300">
-                {" "}
+    
                 {service.id.toString().padStart(2, "0")}{" "}
               </p>{" "}
               <h3 className="text-2xl museo group-hover:scale-110 transition-transform duration-300">
-                {" "}
+    
                 {service.title}{" "}
               </h3>{" "}
             </div>{" "}
             <p className="mont mt-3 text-gray-200 group-hover:text-gray-50 transition-colors duration-300">
-              {" "}
+    
               {service.description}{" "}
             </p>{" "}
           </div>
