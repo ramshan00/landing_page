2b1139587ba8a2769d58c23c61f2a46e3f0a336a
miss noshad <ramshanoshad2@gmail.com>
2025-09-28 05:20:10 +0500

    add link

--- src/screens/homepage/HeroSection/index.tsx
+++ src/screens/homepage/HeroSection/index.tsx
@@ -62,15 +62,17 @@ export default function HeroSection() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.0, ease: "easeOut", delay: 0.8 }}
       >
-        <Button
-          className="font-semibold shadow-lg cursor-pointer bg-gradient-to-r from-[#41dffa] to-[#058bae] text-black mont"
-          whileHover="hover"
-          whileTap="tap"
-          variants={buttonVariant}
-          aria-label="Start Now"
-        >
-          Get Started
-        </Button>
+        <Link href="https://sample-door.vercel.app/signup">
+          <Button
+            className="font-semibold shadow-lg cursor-pointer bg-gradient-to-r from-[#41dffa] to-[#058bae] text-black mont"
+            whileHover="hover"
+            whileTap="tap"
+            variants={buttonVariant}
+            aria-label="Start Now"
+          >
+            Get Started
+          </Button>
+        </Link>
       </motion.div>
 
       {/* Scroll Button */}
